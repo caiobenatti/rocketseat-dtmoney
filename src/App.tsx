4f2956239c51bf5ff4379647cdb1e6920165cbc7
Caio Benatti <caio.benatti@gmail.com>
2021-09-01 00:14:25 +0100

    comments around styled-components for better understanding

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import './App.css';
+import styled from 'styled-components'
+
+const Title = styled.h1` //vc usa aqui a tag HTML que vc quer modificar e usa a maiuscula no nome para nome da constante (best practices)
+color: #8257e6;
+font-size: 64px;
+` //usa aspas para o styled-component ai vc usa o nome da constante como tag de HTML, vc pode usar encadeamento tipo no SASS
 
 export function App() {
   return (
     <div className="App">
-      <h1 className="title">Hello World!</h1>
+      <Title>Hello World</Title>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './App';
+import { App } from './App'; //usa os brackets para importar aqui a funcao App pq vc esta exportando a funcao no App.tsx
 
 ReactDOM.render(
   <React.StrictMode>
