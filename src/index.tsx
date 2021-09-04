53911a96902ed7a2f1d529ec042ba1091dcb8611
Caio Benatti <caio.benatti@gmail.com>
2021-09-04 18:05:30 +0100

    configuring mirageJS

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App'; //usa os brackets para importar aqui a funcao App pq vc esta exportando a funcao no App.tsx
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
+
 
 ReactDOM.render(
   <React.StrictMode>
