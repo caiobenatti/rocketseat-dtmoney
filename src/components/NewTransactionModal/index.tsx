49cb6cf49191c7b9d1e8b1d44c03479309373c84
Caio Benatti <caio.benatti@gmail.com>
2021-09-05 13:20:31 +0100

    adding polished and style the buttons for income and outcome

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from "react-modal";
-import { Container } from "./style";
+import { Container, TransactionTypeContainer } from "./style";
 import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -29,6 +31,18 @@ export function NewTranscactionModal({
         <h2>New Transaction</h2>
         <input placeholder="Title" />
         <input type="number" placeholder="Value" />
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Income icon" />
+            <span>Income</span>
+          </button>
+          <button type="button">
+            <img src={outcomeImg} alt="Outcome icon" />
+            <span>Outcome</span>
+          </button>
+        </TransactionTypeContainer>
+
         <input placeholder="Category" />
         <button type="submit">Done</button>
       </Container>
